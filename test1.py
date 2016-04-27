0ab1413ec21373c89ddc18491d0e7af8c5a00687
David <dkteresi@gmail.com>
2016-04-27 18:16:36 -0400

    Put screen resolution in seperate variable

--- test1.py
+++ test1.py
@@ -1,4 +1,6 @@
 import backend
 
-backend.getImg(backend.getImgUrl((1366, 768), "nature"))
+SCREEN_RESOLUTION = (1366, 768) # put your screen resolution here
+
+backend.getImg(backend.getImgUrl(SCREEN_RESOLUTION, "nature"))
 backend.setBackground()
